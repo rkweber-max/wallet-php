2d74adbeca5debf8f7d4c5909b409a7b17c60ee5
Rodrigo Weber <rodrigo.weber@appmax.com.br>
2026-08-03 22:21:12 -0300

    feat: implement wallet authorization policy for view, update, and delete actions

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -29,25 +29,32 @@ class WalletController extends Controller
 
     public function show(Wallet $wallet)
     {
+        $this->authorize('view', $wallet);
+
         $wallet = $this->walletService->getWallet($wallet);
         return (new WalletResource($wallet))->response()->setStatusCode(200);
     }
 
     public function getTransactions(Wallet $wallet)
     {
+        $this->authorize('view', $wallet);
+
         $transactions = $this->walletService->getTransactions($wallet);
         return TransactionResource::collection($transactions);
     }
 
     public function deposit(Wallet $wallet, TransactionRequest $request)
     {
-        $wallet = $this->walletService->deposit($wallet, $request->input('amount'));
+        $this->authorize('update', $wallet);
 
+        $wallet = $this->walletService->deposit($wallet, $request->input('amount'));
         return (new WalletResource($wallet))->response()->setStatusCode(200);
     }
 
     public function withdraw(Wallet $wallet, TransactionRequest $request)
     {
+        $this->authorize('update', $wallet);
+
         $wallet = $this->walletService->withdraw($wallet, $request->input('amount'));
 
         return (new WalletResource($wallet))->response()->setStatusCode(200);
